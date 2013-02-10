bdb6d84fe057f502e9b1167951befe7668933aef
itanev <iliyan.tanev@gmail.com>
2013-02-10 23:36:33 +0200

    Revert "Revert "test""
    This reverts commit 64a9baa31419539c5e07663d0fbfde5fc3a37793.

---
+++ Arrays/11.BinarySearch/BinarySearch.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class BinarySearch
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Number elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+        //enter the elements
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("El({0}) = ", i);
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        //sort the array
+        Array.Sort(arr);
+
+        //output the sorted array
+        Console.WriteLine("\nThe sorted array: ");
+
+        foreach (int item in arr)
+        {
+            Console.Write("{0} ", item);
+        }
+
+        Console.WriteLine();
+
+        //find element using binary search
+        int result = SearchElementBinary(arr, 0, arr.Length, number);
+
+        if (result == int.MinValue)
+        {
+            Console.WriteLine("Element not found!");
+            return;
+        }
+
+        Console.WriteLine("The element is found at index: {0}", result);
+        
+    }
+
+    //binary search recursivly - wikipedia :P (with litle modification)
+    static int SearchElementBinary(int[] a, int min, int max, int item)
+    {
+        int mid = min + ((max - min) / 2);
+
+        if(mid >= a.Length) 
+        {
+            return int.MinValue;
+        }
+
+        if (a[mid] > item)
+        {
+            return SearchElementBinary(a, min, mid - 1, item);
+        }
+        else if (a[mid] < item)
+        {
+            return SearchElementBinary(a, mid + 1, max, item);
+        }
+        else
+        {
+            return mid;
+        }
+
+    }
+}
+
---
+++ Arrays/14.QuickSort/QuickSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+class QuickSort
+{
+    static void Main()
+    {
+        //I use list becouse I'm lasy :D - it's the same with arrays. 
+        //Only diference is +30 rows of code
+
+        //list of numbers - we could read user input here using for loop
+        List<int> numbers = new List<int> { 1, 2, 54, 67, 3, 8, 2, 89, 6, 3, 76 };
+
+        numbers = SortNumbers(numbers);  //Reusing the same list
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.Write("{0} ", numbers[i]);
+        }
+
+        Console.WriteLine();
+    }
+
+    static List<int> SortNumbers(List<int> sequence)
+    {
+        if (sequence.Count <= 1)
+        {
+            return sequence;
+        }
+        else
+        {
+            int pivot = sequence[sequence.Count/2];
+            sequence.RemoveAt(sequence.Count / 2);
+
+            List<int> less = new List<int>();
+            List<int> greater = new List<int>();
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (sequence[i] < pivot)
+                {
+                    less.Add(sequence[i]);
+                }
+                else
+                {
+                    greater.Add(sequence[i]);
+                }
+            }
+
+            return Concatenate(SortNumbers(less), pivot, SortNumbers(greater));
+        }
+    }
+
+    static List<int> Concatenate(List<int> less, int pivot, List<int> greater)
+    {
+        List<int> result = new List<int>();
+
+        result.AddRange(less);
+        result.Add(pivot);
+        result.AddRange(greater);
+
+        return result;
+    }
+}
