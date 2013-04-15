d15d7a5fd3b4070ffe79ed63ede24fe8c1cd727e
itanev <iliyan.tanev@gmail.com>
2013-04-16 00:11:48 +0300

    adding check to three view js

--- JavaScript/JS part 2/DOM manipulation/06.TreeView/source.js
+++ JavaScript/JS part 2/DOM manipulation/06.TreeView/source.js
@@ -4,13 +4,15 @@ var i = 0;
 
 for(i = 0; i < liTags.length; i++) {
 	liTags[i].addEventListener('click', revealSubList);
-	liTags[i].class = 'expand';
 }
 
 function revealSubList(event) {
-	//event target and 'this' point to the same object 
-	//this === event.target is necessary because otherwise when we click 
-	//on one LI item the event will affect the parent LI as well
+	// check to make shure there is child ul
+	if(typeof this.getElementsByTagName('ul')[0] == 'undefined') return 0;
+	
+	// event target and 'this' point to the same object 
+	// this === event.target is necessary because otherwise when we click 
+	// on one LI item the event will affect the parent LI as well
 	if( this === event.target && 
 		this.getElementsByTagName('ul')[0].style.display == 'block') {
 			this.getElementsByTagName('ul')[0].style.display = 'none';
