bfb72806776e0f598299e3b17fd8d1e168605f33
itanev <iliyan.tanev@gmail.com>
2013-04-16 03:02:39 +0300

    High Quality code variables, expressions, constants

---
+++ High Quality Code/VariablesExpressionsConstants/01.VarsAndExpressions.cs
@@ -0,0 +1,59 @@
+﻿namespace VarsExpressionsConstants
+{
+    using System;
+    using System.Linq;
+
+    public class VarsAndExpressions
+    {
+        static void Main()
+        {
+            //use functions here
+        }
+
+        public static Size GetRotatedSize(Size size, double angleOfRotation)
+        {
+            var angleSinus = Math.Abs(Math.Sin(angleOfRotation));
+            var angleCosinus = Math.Abs(Math.Cos(angleOfRotation));
+
+            var width = angleCosinus * size.Width + angleSinus * size.Height;
+            var height = angleSinus * size.Width + angleCosinus * size.Height;
+
+            return new Size(width, height);
+        }
+    }
+
+    public class Size
+    {
+        private double width, height;
+
+        public double Width 
+        {
+            get 
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+ 
+        public double Height 
+        {
+            get 
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
---
+++ High Quality Code/VariablesExpressionsConstants/02.VarsAndExpressions.cs
@@ -0,0 +1,63 @@
+﻿namespace _02.VarsAndExpressions
+{
+    using System;
+    using System.Linq;
+
+    public class VarsAndExpressions
+    {
+        static void Main()
+        {
+            //use functions
+        }
+
+        public void PrintStatistics(double[] array)
+        {
+            var count = array.Length;
+
+            double max = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            PrintValue(max);
+
+            double min = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            PrintValue(min);
+
+            double sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                sum += array[i];
+            }
+
+            PrintAvg(sum, count);
+        }
+
+        private void PrintAvg(double sum, int count)
+        {
+            var average = sum / count;
+
+            PrintValue(average);
+        }
+
+        private void PrintValue(double value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
