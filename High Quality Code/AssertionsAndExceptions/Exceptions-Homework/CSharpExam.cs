caa2117ee3c961ef36275f3cde859ff558cf3af1
itanev <iliyan.tanev@gmail.com>
2013-04-28 17:24:40 +0300

    assertions and exceptions

---
+++ High Quality Code/AssertionsAndExceptions/Exceptions-Homework/CSharpExam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class CSharpExam : Exam
+{
+    private int score;
+
+    public CSharpExam(int score)
+    {
+        this.Score = score;
+    }
+
+    public int Score
+    {
+        get
+        {
+            return this.score;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Score must be positive!");
+            }
+
+            this.score = value;
+        }
+    }
+
+    public override ExamResult Check()
+    {
+        if (Score < 0 || Score > 100)
+        {
+            throw new ArgumentOutOfRangeException("Score is invalid! It must be between 0 and 100.");
+        }
+        else
+        {
+            return new ExamResult(this.Score, 0, 100, "Exam results calculated by score.");
+        }
+    }
+}
---
+++ High Quality Code/AssertionsAndExceptions/Exceptions-Homework/ExamResult.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class ExamResult
+{
+    private int grade;
+    private int minGrade;
+    private int maxGrade;
+    private string comments;
+
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade 
+    {
+        get
+        {
+            return this.grade;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Grade must be positive!");
+            }
+
+            this.grade = value;
+        }
+    }
+
+    public int MinGrade 
+    {
+        get
+        {
+            return this.minGrade;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Min Grade must be positive!");
+            }
+
+            this.minGrade = value;
+        }
+    }
+
+    public int MaxGrade 
+    {
+        get
+        {
+            return this.maxGrade;
+        }
+
+        private set
+        {
+            if (value >= this.minGrade)
+            {
+                throw new ArgumentException("Max grade must be gratter than min grade!");
+            }
+
+            this.maxGrade = value;
+        }
+    }
+
+    public string Comments 
+    {
+        get
+        {
+            return this.comments;
+        }
+
+        private set
+        {
+            if (value == string.Empty || value == null)
+            {
+                throw new ArgumentException("Comments must have content!");
+            }
+
+            this.comments = value;
+        }
+    }
+}
