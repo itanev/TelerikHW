374b78971178541dcbe2eff29746dc6a9038dbe7
itanev <iliyan.tanev@gmail.com>
2013-05-14 01:40:27 +0300

    Development Tools HW

---
+++ High Quality Code/DevelopmentTools/T4 Template-Log4Net/Log4Net/LogTest.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using log4net.Config;
+
+public class LogTest
+{
+    /// <summary>
+    /// You can see the configuration file Log4Net.exe.config in the bin directory
+    /// LogTest2.txt is the file where the loged information is saved.
+    /// The logger also prints the information on the console.
+    /// This is all configured in the configuration file.
+    /// </summary>
+    private static readonly ILog logger = LogManager.GetLogger(typeof(LogTest));
+
+    static LogTest()
+    {
+        DOMConfigurator.Configure();
+    }
+
+    static void Main()
+    {   
+        logger.Debug("Here is a debug log.");
+        logger.Info("... and an Info log.");
+        logger.Warn("... and a warning.");
+        logger.Error("... and an error.");
+        logger.Fatal("... and a fatal error.");
+    }
+}
