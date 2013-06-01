c505f0dd7c944c3f835d7df6f9c35f633af50e5b
itanev <iliyan.tanev@gmail.com>
2013-06-01 20:50:18 +0300

    new ex-es

---
+++ Data Structures & Algorithms/LinearDataStructures/07.FindIndegersInRangeAndNumOccurences/FindIndegersInRangeAndNumOccurences.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.FindIndegersInRangeAndNumOccurences
+{
+    public class FindIndegersInRangeAndNumOccurences
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int>();
+
+            int currNumber = 0;
+            bool isValidNumber = false;
+
+            Console.WriteLine("Enter some numbers. Enter invalid number to stop entering.");
+
+            do
+            {
+                Console.Write("Enter number: ");
+                isValidNumber = int.TryParse(Console.ReadLine(), out currNumber);
+                numbers.Add(currNumber);
+            }
+            while (isValidNumber);
+
+            Dictionary<int, int> result = FindNumbersInRangeAndTheirOccurrences(numbers.GetRange(0, numbers.Count - 1));
+
+            PrintNumbersAndOccurrences(result);
+        }
+
+        private static void PrintNumbersAndOccurrences(Dictionary<int, int> numbersAndOccurrences)
+        {
+            foreach (var number in numbersAndOccurrences)
+            {
+                Console.WriteLine("{0} -> {1}", number.Key, number.Value);
+            }
+        }
+
+        private static Dictionary<int, int> FindNumbersInRangeAndTheirOccurrences(List<int> numbers)
+        {
+            Dictionary<int, int> resultNumbersAndOccurrences = new Dictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (number >= 0 && number <= 1000) continue;
+
+                if (resultNumbersAndOccurrences.ContainsKey(number))
+                {
+                    resultNumbersAndOccurrences[number]++;
+                }
+                else
+                {
+                    resultNumbersAndOccurrences.Add(number, 1);
+                }
+            }
+
+            return resultNumbersAndOccurrences;
+        }
+    }
+}
---
+++ Data Structures & Algorithms/LinearDataStructures/08.FindMajorant/FindMajorant.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.FindMajorant
+{
+    public class FindMajorant
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int>();
+
+            int currNumber = 0;
+            bool isValidNumber = false;
+
+            Console.WriteLine("Enter some numbers. Enter invalid number to stop entering.");
+
+            do
+            {
+                Console.Write("Enter number: ");
+                isValidNumber = int.TryParse(Console.ReadLine(), out currNumber);
+                numbers.Add(currNumber);
+            }
+            while (isValidNumber);
+
+            int majorant = GetMajorant(numbers.GetRange(0, numbers.Count - 1));
+
+            if (majorant != int.MinValue)
+            {
+                Console.WriteLine("Majorant: {0}", majorant);
+            }
+            else
+            {
+                Console.WriteLine("No majorant.");
+            }
+        }
+
+        private static int GetMajorant(List<int> numbers)
+        {
+            Dictionary<int, int> numbersAndOccurrences = new Dictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (numbersAndOccurrences.ContainsKey(number))
+                {
+                    numbersAndOccurrences[number]++;
+                }
+                else
+                {
+                    numbersAndOccurrences.Add(number, 1);
+                }
+            }
+
+            int maxOccuringElement = numbersAndOccurrences.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            int atLeastOccurrences = numbers.Count / 2 + 1;
+
+            if(numbersAndOccurrences[maxOccuringElement] >= atLeastOccurrences)
+                return maxOccuringElement;
+
+            return int.MinValue;
+        }
+    }
+}
