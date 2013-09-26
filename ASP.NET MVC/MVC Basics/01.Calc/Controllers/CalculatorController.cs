05b9e333a1d93d3419f724adff44c0119ff48bf3
itanev <iliyan.tanev@gmail.com>
2013-09-26 10:48:41 +0300

    asp.net mvc - essentials
    work in progress

---
+++ ASP.NET MVC/MVC Basics/01.Calc/Controllers/CalculatorController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _01.Calc.ViewModels;
+
+namespace _01.Calc.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private List<string> types = new List<string>()
+                { 
+                    "bit", "byte", "kilobit", "kilobyte", "megabit", "megabyte", "gigabit",
+                    "gigabyte", "terabit", "terabyte", "petabit", "petabyte", "exabit", "exabyte",
+                    "zettabit", "zettabyte", "yottabit", "yottabyte"
+                };
+
+        private Dictionary<string, decimal> bitTypesSize = new Dictionary<string, decimal>();
+
+        public ActionResult Index()
+        {
+            FillWithZeroes();
+            ViewBag.typesSizes = this.bitTypesSize;
+
+            return View();
+        }
+
+        [ActionName("Calculate")]
+        [HttpPost]
+        public ActionResult Calc(Data parameters)
+        {
+            FillWithZeroes();
+
+            var enteredType = parameters.Type.ToLower().Trim();
+            var currQuantity = 0;
+
+            int kilo = 1024;
+            if (parameters.Kilo != 0)
+            {
+                kilo = parameters.Kilo;
+            }
+
+            if (this.bitTypesSize.ContainsKey(enteredType))
+            {
+                currQuantity = parameters.Quantity;
+            }
+
+            decimal bits = CalcBits(enteredType, currQuantity, kilo);
+
+            this.bitTypesSize[this.types[0]] = bits;
+            this.bitTypesSize[this.types[1]] = bits / 8;
+            this.bitTypesSize[this.types[2]] = bits / kilo;
+
+            FillBits(kilo);
+                
+            ViewBag.typesSizes = this.bitTypesSize;
+
+            return View("Index");
+        }
+
+        private void FillWithZeroes()
+        {
+            foreach (var type in this.types)
+            {
+                this.bitTypesSize[type] = 0m;
+            }
+        }
+
+        private void FillBits(int kilo)
+        {
+            for (int i = 3; i < this.types.Count; i++)
+            {
+                var prevVal = this.bitTypesSize[this.types[i - 2]];
+                this.bitTypesSize[this.types[i]] = prevVal / kilo;
+            }
+        }
+
+        private decimal CalcBits(string enteredType, decimal bits, int kilo)
+        {
+            if (enteredType != this.types[0])
+            {
+                int pos = this.types.IndexOf(enteredType) + 1;
+
+                for (int i = pos; i > 0; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        bits *= 8;
+                    }
+                    else
+                    {
+                        bits *= kilo;
+                    }
+                }
+            }
+
+            return bits;
+        }
+    }
+}
---
+++ ASP.NET MVC/MVC Basics/01.Calc/ViewModels/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _01.Calc.ViewModels
+{
+    public class Data
+    {
+        public int Quantity { get; set; }
+        public string Type { get; set; }
+        public int Kilo { get; set; }
+    }
+}
